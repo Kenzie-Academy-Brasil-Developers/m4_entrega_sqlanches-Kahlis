73c602c000a1d0c469e2639d17d540d1d7ff7e4c
Matheus de Souza Arruda <kahlis.dev@gmail.com>
2022-08-15 07:12:09 -0300

    feat: Exercício finalizado

--- crud.sql
+++ crud.sql
@@ -4,31 +4,75 @@
 -- Criação
 
 -- 1)
-
+INSERT INTO clientes
+	(nome, lealdade)
+VALUES
+	('Georgia', 0);
 
 
 -- 2)
-
-
+INSERT INTO pedidos
+	(status, cliente_id)
+VALUES
+	('Recebido', currval('clientes_id_seq'));
 
 -- 3)
-
-
+INSERT INTO produtos_pedidos
+	(pedido_id, produto_id)
+VALUES
+	(currval('pedidos_id_seq'), 1),
+	(currval('pedidos_id_seq'), 2),
+	(currval('pedidos_id_seq'), 6),
+	(currval('pedidos_id_seq'), 8),
+	(currval('pedidos_id_seq'), 8);
 
 -- Leitura
 
 -- 1)
+SELECT 
+	pedido.id,
+	pedido.status,
+    pedido.cliente_id,
+    produto.id,
+    produto.nome,
+    produto.tipo,
+    produto.preço,
+    produto.pts_de_lealdade
+FROM
+	produtos_pedidos at
+JOIN
+	pedidos pedido ON pedido.id = at.pedido_id
+JOIN
+	produtos produto ON produto.id = at.produto_id
+WHERE
+	pedido.cliente_id = 12;
 
 
 
 -- Atualização
 
 -- 1)
-
-
-
+UPDATE 
+	clientes
+SET 
+	lealdade=
+    (
+		SELECT
+      		SUM(produto.pts_de_lealdade)
+      	FROM
+      		produtos_pedidos at
+      	JOIN 
+    		produtos produto ON produto.id = at.produto_id
+		JOIN
+			pedidos pedido ON pedido.id = at.pedido_id
+        WHERE
+            pedido.cliente_id = 12
+	)
+WHERE
+	clientes.id = 12;
+	
 -- Deleção
 
 -- 1)
-
+DELETE FROM clientes WHERE clientes.id = 4;
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,66 @@
 -- Seleções de dados
 
 -- 1)
+SELECT 
+	pedido.id,
+	pedido.status,
+    pedido.cliente_id,
+    produto.id,
+    produto.nome,
+    produto.tipo,
+    produto.preço,
+    produto.pts_de_lealdade
+FROM
+	produtos_pedidos at
+JOIN
+	pedidos pedido ON pedido.id = at.pedido_id
+JOIN
+	produtos produto ON produto.id = at.produto_id;
 
 -- 2)
+SELECT pedido_id FROM produtos_pedidos WHERE produto_id = 6;
 
 -- 3)
+SELECT 
+	cliente.nome gostam_de_fritas
+FROM 
+	produtos_pedidos at
+JOIN
+	pedidos pedido ON pedido.id = at.pedido_id
+JOIN
+	clientes cliente ON cliente.id = pedido.cliente_id
+WHERE
+	produto_id = 6;
 
 -- 4)
+SELECT 
+	SUM(produto.preço)
+FROM 
+	produtos_pedidos at
+JOIN
+	produtos produto ON produto.id = at.produto_id
+JOIN
+	pedidos pedido ON pedido.id = at.pedido_id
+JOIN
+	clientes cliente ON cliente.id = pedido.cliente_id
+WHERE
+	cliente.id = 5;
+    
+SELECT * FROM pedidos;
 
 -- 5)
+SELECT 
+	nome,
+    quantidade
+FROM
+(
+	SELECT
+      	produto.nome,
+      	(SELECT COUNT(*) FROM produtos_pedidos WHERE produto_id = produto.id) quantidade,
+		COUNT(*) OVER (PARTITION BY produto.nome) AS cnt
+	FROM
+		produtos_pedidos at
+	JOIN
+		produtos produto ON produto.id = at.produto_id
+) as p
+GROUP BY nome, quantidade;
